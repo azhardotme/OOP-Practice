cc65ce757921cb90995b96a3ea8d66e6b033c939
Azharul Islam <32319631+azhardotme@users.noreply.github.com>
2019-08-02 20:35:42 +0600

    Add files via upload

---
+++ Methods/Main.java
@@ -0,0 +1,31 @@
+
+package day2;
+
+/**
+ *
+ * @author Azhar
+ */
+public class Main {
+    public static void main(String[] args) {
+        Teacher teacher1 = new Teacher();
+        
+        teacher1.name = "Ataus Samad";
+        teacher1.age=28;
+        teacher1.dept ="CSE";
+        
+        
+        teacher1.displayinfo();
+        
+        
+         Teacher teacher2 = new Teacher();
+        
+        teacher2.name = "Habibur Rahman";
+        teacher2.age=30;
+        teacher2.dept ="CSE";
+        
+        
+        teacher2.displayinfo();
+        
+    }
+    
+}
---
+++ Methods/Teacher.java
@@ -0,0 +1,20 @@
+
+package day2;
+
+/**
+ *
+ * @author Azhar
+ */
+public class Teacher {
+    String name,dept;
+    int age;
+    
+    void displayinfo(){
+        System.out.println("Name : "+name);
+        System.out.println("Age: "+age);
+        System.out.println("Department : "+dept);
+        
+    }
+    
+    
+}
